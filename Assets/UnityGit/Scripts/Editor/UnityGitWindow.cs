d8a914f4c528724f55a1831e174c88113c9606c9
Tiralus <skylander@gmail.com>
2018-07-08 22:52:41 -0700

    Add start of editor window

---
+++ Assets/UnityGit/Scripts/Editor/UnityGitWindow.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace UnityGit
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEditor;
+
+	public class UnityGitWindow : EditorWindow
+	{
+		[MenuItem("Window/Unity Git")]
+		public static void ShowWindow()
+		{
+			EditorWindow.GetWindow(typeof(UnityGitWindow));
+		}
+
+		public void OnGUI()
+		{
+			
+		}
+	}
+}
